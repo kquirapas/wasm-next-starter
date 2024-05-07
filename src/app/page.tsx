272d8e9a94b747853083fc2b086e064bc08ed935
Kristian Quirapas <kmquirapas@gmail.com>
2024-05-07 21:49:31 +0800

    fix: removed client side error on no phantom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 "use client"; // Prevents the useWasm is not a function error
+import { useState } from "react";
 import { useWasm } from "@/modules/wasm";
 
 export default function Home() {
   const { add, getPhantom, isLoading } = useWasm();
+
+  const checkPhantom = () => {
+    if (isLoading) return;
+    try {
+      let phantom = getPhantom();
+      let isPhantom = phantom.solana.isPhantom;
+      return isPhantom ? "Yes Phantom" : "No Phantom";
+    } catch (err) {
+      console.error(err);
+      return "No Phantom";
+    }
+  };
+
   return (
     <main className="text-green-500">
       {isLoading ? (
@@ -10,7 +24,7 @@ export default function Home() {
       ) : (
         <div>
           <span>{`1 + 1 = ${add(1, 1)}`}</span>
-          <div>{String(getPhantom().solana?.isPhantom)}</div>
+          <div>{checkPhantom()}</div>
         </div>
       )}
     </main>
